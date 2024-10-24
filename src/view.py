1c53e729c2660da2caad8953914474f9482649af
Fabian Joos <bubenlouis@gmail.com>
2024-10-24 14:18:16 +0200

    Add Tkinter GUI elements to TcView class

---
+++ src/view.py
@@ -0,0 +1,27 @@
+#import tkinter as tk
+from tkinter import ttk
+
+class TcView():
+    def __init__(self, root):
+        self.root = root
+        self.setup_gui()
+
+    def setup_gui(self):
+        self.setup_window()
+        self.init_widgets()
+        self.place_widgets()
+
+    
+    def setup_window(self):
+        self.root.title("Title - TcView v0.1")
+        self.root.geometry("800x600")
+
+    def init_widgets(self):
+        self.label = ttk.Label(self.root, text="Hello, Tkinter!")
+        self.button = ttk.Button(self.root, text="Click me!")
+        self.entry = ttk.Entry(self.root)
+
+    def place_widgets(self):
+        self.label.pack(side="top")
+        self.button.pack(side="top")
+        self.entry.pack(side="top", fill="x", padx=10)
