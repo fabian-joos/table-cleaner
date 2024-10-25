94af101a58ae6d4a6b01d3d6deb69fe315343f1e
Fabian Joos <bubenlouis@gmail.com>
2024-10-25 12:59:49 +0200

    Refactor TcController __init__ method to accept a model parameter

--- src/controller.py
+++ src/controller.py
@@ -1,10 +1,13 @@
 class TcController():
-    def __init__(self, view):
+    def __init__(self, view, model):
         self.view = view
+        self.model = model
         self.setup()
 
     def setup(self):
         self.view.load_file_button.config(command=self.load_file_button_click)
 
     def load_file_button_click(self):
-        print("Load file button clicked!")
+        self.model.load_file()
+        self.view.load_file_entry.delete(0, "end")
+        self.view.load_file_entry.insert(0, self.model.load_file_filename)
