290993575ceba179da0aeb59bdca200d47fffbc9
Fabian Joos <bubenlouis@gmail.com>
2024-10-26 00:12:11 +0200

    Refactor TcController load_file_button_click method Refactor Tkinter GUI initialization and widget placement Implement save_file_button_click method in TcController Implement replace_button_click method in TcController Implement lookup_expression_check_click method in TcController Implement save_file method in TcModel Implement replace method in TcModel Update view.py to include new widgets and functionality

--- src/controller.py
+++ src/controller.py
@@ -6,6 +6,10 @@ class TcController():
 
     def setup(self):
         self.view.load_file_button.config(command=self.load_file_button_click)
+        self.view.save_file_button.config(command=self.save_file_button_click)
+        self.view.lookup_column_combobox.config(values=self.model.df_columns)
+        self.view.lookup_expression_check.config(command=self.lookup_expression_check_click)
+        self.view.replace_button.config(command=self.replace_button_click)
 
     def load_file_button_click(self):
         self.model.load_file()
@@ -13,3 +17,20 @@ class TcController():
         self.view.load_file_entry.insert(0, self.model.load_file_filename)
         self.view.load_file_status.config(text=f"File loaded, shape: {self.model.df.shape}")
         self.view.lookup_column_combobox.config(values=self.model.df_columns)
+
+    def save_file_button_click(self):
+        self.model.save_file()
+        self.view.save_file_status.config(text="File saved")
+    
+    def replace_button_click(self):
+        lookup_column = self.view.lookup_column_combobox.get()
+        lookup_expression = self.view.lookup_expression_entry.get()
+        replace_expression = self.view.replace_expression_entry.get()
+        self.model.replace(lookup_column, lookup_expression, replace_expression)
+        self.view.replace_status.config(text="Replacements made")
+
+    def lookup_expression_check_click(self):
+        lookup_column = self.view.lookup_column_combobox.get()
+        lookup_expression = self.view.lookup_expression_entry.get()
+        lookup_result = self.model.df[lookup_column].str.contains(lookup_expression)
+        self.view.lookup_expression_status.config(text=f"Lookup expression found in {lookup_result.sum()} rows")
--- src/model.py
+++ src/model.py
@@ -15,3 +15,13 @@ class TcModel():
         if self.load_file_filename:
             self.df = pd.read_csv(self.load_file_filename, sep=";", encoding="utf-8")
             self.df_columns = list(self.df.columns)
+
+    def save_file(self):
+        save_file_filename = filedialog.asksaveasfilename(
+            defaultextension=".csv",
+            filetypes=[("CSV files", "*.csv")])
+        self.df.to_csv(save_file_filename, sep=";", encoding="utf-8", index=False)
+
+    def replace(self, lookup_column, lookup_expression, replace_expression):
+        if lookup_column in self.df_columns:
+            self.df[lookup_column] = self.df[lookup_column].str.replace(lookup_expression, replace_expression)
