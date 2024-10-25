3e247448b9f85b952f39044f677f5bb2de0cae66
Fabian Joos <bubenlouis@gmail.com>
2024-10-26 00:16:56 +0200

    Added docstrings to TcModel methods

--- src/model.py
+++ src/model.py
@@ -9,6 +9,7 @@ class TcModel():
         self.df_columns = []
 
     def load_file(self):
+        '''Load CSV file to DataFrame and update df_columns'''
         self.load_file_filename = filedialog.askopenfilename(
             defaultextension=".csv",
             filetypes=[("CSV files", "*.csv")])
@@ -17,11 +18,13 @@ class TcModel():
             self.df_columns = list(self.df.columns)
 
     def save_file(self):
+        '''Save DataFrame to CSV file'''
         save_file_filename = filedialog.asksaveasfilename(
             defaultextension=".csv",
             filetypes=[("CSV files", "*.csv")])
         self.df.to_csv(save_file_filename, sep=";", encoding="utf-8", index=False)
 
     def replace(self, lookup_column, lookup_expression, replace_expression):
+        '''Replace lookup_expression with replace_expression in lookup_column of DataFrame df'''
         if lookup_column in self.df_columns:
             self.df[lookup_column] = self.df[lookup_column].str.replace(lookup_expression, replace_expression)
